fe55f4af7e123bb9cdd553c876c79f346aae7d1e
ClausClan <160112433+ClausClan@users.noreply.github.com>
2025-11-01 11:49:17 +0100

    Opdaterede logo referancer og stier

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
 // Definer et unikt navn og version til din cache
-const CACHE_NAME = 'min-madplan-cache-v1';
+const CACHE_NAME = 'min-madplan-cache-v2'; // Opdateret version!
 
-// Definer de filer, der skal caches. 
-// Da din app er én fil, er det bare rod-stien ('/') og selve filen.
+// RETTET: Alle stier skal være relative (./) og inkludere ALLE app-filer
 const URLS_TO_CACHE = [
-    '/',
-    '/index.html' 
-    // Hvis du havde ikoner til din PWA, ville de blive tilføjet her
-    // f.eks. '/icon-192.png'
+    './',
+    './index.html',
+    './manifest.json',
+    './Logo.png',
+    './Logo1.png',
+    './Logo2.png',
+    './Flavicon.ico'
 ];
 
 // --- 1. INSTALL Event: Henter og gemmer filerne i cachen ---
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installerer...');
+    console.log('Service Worker: Installerer v2...');
     
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -29,7 +31,7 @@ self.addEventListener('install', (event) => {
 
 // --- 2. ACTIVATE Event: Rydder op i gamle caches ---
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Aktiverer...');
+    console.log('Service Worker: Aktiverer v2...');
     
     event.waitUntil(
         caches.keys().then((cacheNames) => {
@@ -51,21 +53,15 @@ self.addEventListener('activate', (event) => {
 
 // --- 3. FETCH Event: Intercepterer netværkskald ---
 self.addEventListener('fetch', (event) => {
-    // Vi bruger en "Cache First"-strategi
-    // 1. Prøv at finde ressourcen i cachen
-    // 2. Hvis ikke fundet, hent den fra netværket
-    
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
                 // Hvis response findes i cachen, returner den
                 if (response) {
-                    console.log('Service Worker: Serverer fra cache:', event.request.url);
                     return response;
                 }
 
                 // Ellers, hent fra netværket
-                console.log('Service Worker: Henter fra netværk:', event.request.url);
                 return fetch(event.request);
             })
     );
